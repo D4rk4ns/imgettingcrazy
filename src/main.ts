1cf7a074b5357c4bae74bc3e7c00caa6e839fd90
D4rk4ns <jonathanlsr9611@gmail.com>
2022-09-12 16:11:46 -0400

    Forget a variable (T_T)'

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env. || 3000;
+  const port = process.env.APP_PORT || 3000;
 
   app.useGlobalPipes(new ValidationPipe());
 
